31d6f28cc5ae4307cea0210c082c933d717654a2
Hari <harikanth664@gmail.com>
2023-08-08 11:29:29 +0530

    Video on Routes complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import './App.css';
 import Alerts from './components/Alerts';
 import Navbar from './components/Navbar';
+import About from './components/About';
 import TextForm from './components/TextForm';
-// import { ReactDOM } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type, fade)=> {
+  const showAlert = (message, type, fade) => {
     setAlert({
       msg: message,
       type: type
@@ -22,12 +24,16 @@ function App() {
 
   return (
     <>
-      <Navbar title="TextUtils" aboutTitle="About"/>
-      <Alerts alertMessage={alert}/>
-      <div className="container">
-      <TextForm showAlert={showAlert} textTitle="Enter the text to analyze below"/>
-      </div>
-      {/* <About/> */}
+      <Router>
+        <Navbar title="TextUtils" aboutTitle="About" />
+        <Alerts alertMessage={alert} />
+        <div className="container my-3">
+        <Routes>
+            <Route exact path="/" element={<TextForm showAlert={showAlert} textTitle="Enter the text to analyze below" />} />
+            <Route exact path="/about" element={<About/>} /> 
+        </Routes>
+        </div>
+      </Router>
     </>
   );
 }
